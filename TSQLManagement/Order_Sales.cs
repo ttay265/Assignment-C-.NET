c9b180bdd577c99a15f7d96024430dbbad3310ba
ttay265 <Dangerfi1>
2016-08-11 12:44:46 +0700

    Upload Form mau

---
+++ TSQLManagement/Order_Sales.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TSQLManagement
+{
+    public partial class Order_Sales : Form
+    {
+        public Order_Sales()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Search_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
