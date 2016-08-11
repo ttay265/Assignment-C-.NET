3c678d1ba882579c633ae71ccded70ca6ba8af57
ttay265 <Dangerfi1>
2016-08-11 22:10:55 +0700

    Upload entity framework 5.0

--- TSQLManagement/Form1.Designer.cs
+++ TSQLManagement/Form1.Designer.cs
@@ -169,7 +169,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(706, 426);
+            this.ClientSize = new System.Drawing.Size(570, 303);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.dgvDataList);
             this.Controls.Add(this.panel1);
