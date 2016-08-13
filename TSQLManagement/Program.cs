d31dba3c5d004dd34f64b81728535ed56e6c10d9
damtiennam <south.32.1995@gmail.com>
2016-08-13 15:01:02 +0700

    Nam lam add,new Product

--- TSQLManagement/Program.cs
+++ TSQLManagement/Program.cs
@@ -16,7 +16,8 @@ namespace TSQLManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new OrderDetailsForm());
+            Application.Run(new ProductsForm());
         }
     }
 }
